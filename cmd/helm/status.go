d66fd3870efa85bc317d96de488c5e4cb219c400
Vaughn Dice <vadice@microsoft.com>
2019-07-16 13:29:52 -0600

    chore(vendor): update porter dep

--- cmd/helm/status.go
+++ cmd/helm/status.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"github.com/deislabs/porter-helm/pkg/helm"
-	"github.com/deislabs/porter/pkg/printer"
 	"github.com/spf13/cobra"
 )
 
 func buildStatusCommand(m *helm.Mixin) *cobra.Command {
-	opts := struct {
-		rawFormat string
-		format    printer.Format
-	}{}
+	opts := helm.StatusOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "status",
 		Short: "Print the status of the helm components in the bundle",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-			opts.format, err = printer.ParseFormat(opts.rawFormat)
-			return err
+			return opts.ParseFormat()
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return m.Status(printer.PrintOptions{Format: opts.format})
+			return m.Status(opts)
 		},
 	}
 
-	cmd.Flags().StringVarP(&opts.rawFormat, "output", "o", "plaintext", "Output format. Allowed values are: plaintext, yaml, json")
+	cmd.Flags().StringVarP(&opts.RawFormat, "output", "o", "plaintext", "Output format. Allowed values are: plaintext, yaml, json")
 	return cmd
 }
