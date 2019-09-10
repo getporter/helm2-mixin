890788ecda3cec9ef887c93021a17e211bbe9235
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-10 14:39:02 -0500

    Support version --output flag

--- pkg/helm/version.go
+++ pkg/helm/version.go
@@ -1,11 +1,19 @@
 package helm
 
 import (
-	"fmt"
-
 	"github.com/deislabs/porter-helm/pkg"
+	"github.com/deislabs/porter/pkg/mixin"
+	"github.com/deislabs/porter/pkg/porter/version"
 )
 
-func (m *Mixin) PrintVersion() {
-	fmt.Fprintf(m.Out, "helm mixin %s (%s)\n", pkg.Version, pkg.Commit)
+func (m *Mixin) PrintVersion(opts version.Options) error {
+	metadata := mixin.Metadata{
+		Name: "helm",
+		VersionInfo: mixin.VersionInfo{
+			Version: pkg.Version,
+			Commit:  pkg.Commit,
+			Author:  "DeisLabs",
+		},
+	}
+	return version.PrintVersion(m.Context, opts, metadata)
 }
